f5483d1676d7f2346ee586da93ae989623cb2a89
Andrewwango <andrew.wang27@gmail.com>
2016-04-30 17:55:57 +0100

    Update ustest.py

--- ustest.py
+++ ustest.py
@@ -1,11 +1,11 @@
 from BrickPi import *
 
 BrickPiSetup()
-BrickPi.SensorType[PORT_1] = TYPE_SENSOR_ULTRASONIC_CONT
+BrickPi.SensorType[PORT_3] = TYPE_SENSOR_ULTRASONIC_CONT
 BrickPiSetupSensors()   #Send the properties of sensors to BrickPi
 
 while True:
     result = BrickPiUpdateValues()  # Ask BrickPi to update values for sensors/motors 
     if not result :
-        print BrickPi.Sensor[PORT_1]     #BrickPi.Sensor[PORT] stores the value obtained from sensor
+        print BrickPi.Sensor[PORT_3]     #BrickPi.Sensor[PORT] stores the value obtained from sensor
     time.sleep(.01)     # sleep for 10 ms
