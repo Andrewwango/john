263ba3728419ff789fe020304bfcc92cc13dcc22
Andrewwango <andrew.wang27@gmail.com>
2016-04-30 21:42:17 +0100

    Update ustest.py

--- ustest.py
+++ ustest.py
@@ -9,4 +9,4 @@ while True:
     result = BrickPiUpdateValues()  # Ask BrickPi to update values for sensors/motors 
     if not result :
         print BrickPi.Sensor[PORT_3]     #BrickPi.Sensor[PORT] stores the value obtained from sensor
-    time.sleep(.01)     # sleep for 10 ms
+    time.sleep(.05)     # sleep for 10 ms
