1f2b6fd960e01e434916fb5aa34cfa668654bef8
Andrewwango <andrew.wang27@gmail.com>
2016-04-30 16:23:06 +0100

    Update test.py

--- test.py
+++ test.py
@@ -12,17 +12,25 @@ BrickPi.MotorEnable[arm] = 1
 power = 200 #speed from -255 to 255
 
 #this is close
+print "starting to close"
 BrickPi.MotorSpeed[grabber] = 80
 BrickPiUpdateValues()
+print "closing"
 time.sleep(0.3)
+print "finished waiting, stopping"
 BrickPi.MotorSpeed[grabber] = 0
 BrickPiUpdateValues()
+print "waiting"
 time.sleep(5)
 
 #this is open
+print "starting to open"
 BrickPi.MotorSpeed[grabber] = -80
 BrickPiUpdateValues()
+print "opening"
 time.sleep(0.3)
+print "finished waiting, stopping"
 BrickPi.MotorSpeed[grabber] = 0
 BrickPiUpdateValues()
-time.sleep(2)
+print "waiting"
+time.sleep(5)
