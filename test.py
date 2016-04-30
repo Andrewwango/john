02129fb20364e5b8dd97d14624fdde47a01fcc5b
Andrewwango <andrew.wang27@gmail.com>
2016-04-30 16:06:55 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,26 @@
-print "hello"
-print "boo"
-print "yay"
+from BrickPi import *
+import time
+BrickPiSetup()
+BrickPiSetupSensors()
+
+grabber = PORT_D
+BrickPi.MotorEnable[grabber] = 1
+arm = PORT_A
+BrickPi.MotorEnable[arm] = 1
+
+
+power = 200 #speed from -255 to 255
+
+BrickPi.MotorSpeed[grabber] = 20
+BrickPiUpdateValues()
+time.sleep(2)
+BrickPi.MotorSpeed[grabber] = 0
+BrickPiUpdateValues()
+time.sleep(2)
+
+BrickPi.MotorSpeed[grabber] = -20
+BrickPiUpdateValues()
+time.sleep(2)
+BrickPi.MotorSpeed[grabber] = 0
+BrickPiUpdateValues()
+time.sleep(2)
