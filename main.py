594afd508e04ffd400ef826546996ee8f1ba2c7c
Andrewwango <andrew.wang27@gmail.com>
2016-05-07 21:58:52 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,19 +2,22 @@ from BrickPi import *
 import time
 BrickPiSetup()
 
-#if motors stalling - check voltage
+##DEBUG##
+#if motors stalling - check voltage + PSU
 #brickpiupdatevalues error - do system thingy
 #if result=-1 - reboot
 
+##NOTES##
+#default position is opened grabber, half down	
+#positive speed = rolling away from bum
 
-#define motors
-#pos speed = rolling away from bum
+##CONSTANTS##
 LWHEEL = PORT_D
 RWHEEL = PORT_A
 GRABBER = PORT_B
 ARM = PORT_C
 HEAD = PORT_1
-USSTANDARD = 25 #us sensor reading of floor
+USSTANDARD = 25 #us sensor threshold
 WHEELPOWER     = -100
 GRABBERPOWER   =  100
 LIFTPOWER      = -170
@@ -23,6 +26,7 @@ OPENPOWER      = -40
 BRINGDOWNPOWER =  150
 
 
+##SETUP##
 BrickPi.MotorEnable[GRABBER] = 1
 BrickPi.MotorEnable[ARM] = 1
 BrickPi.MotorEnable[LWHEEL] = 1
@@ -31,6 +35,8 @@ BrickPiSetupSensors()
 BrickPi.SensorType[HEAD] = TYPE_SENSOR_ULTRASONIC_CONT
 BrickPiSetupSensors()
 
+
+##FUNCTIONS##
 def movelimb(limb, speed, length, grabberaswell=0):
 		BrickPi.MotorSpeed[limb] = speed
 		if grabberaswell==1:
@@ -40,66 +46,64 @@ def movelimb(limb, speed, length, grabberaswell=0):
 			BrickPiUpdateValues()
 		time.sleep(.1)
 
-#default pos is opened grabber, half down	
-
-while True:
-	#move wheels
-	BrickPi.MotorSpeed[LWHEEL] = WHEELPOWER
-	BrickPi.MotorSpeed[RWHEEL] = WHEELPOWER
-	
-	#get us reading
+def takeusreading():
 	uslist=[]
 	for i in range(7):
 		result = BrickPiUpdateValues()
 		print result
-		if not result :
+		if not result:
 			uslist += [int(BrickPi.Sensor[HEAD])]
 		time.sleep(.05)
 	print uslist
 	usreading = max(set(uslist), key=uslist.count) #mode
 	print "usreading is " + str(usreading)
-	
+	return usreading
+
+
+##MAIN LOOP##
+while True:
+	#move wheels
+	BrickPi.MotorSpeed[LWHEEL] = WHEELPOWER
+	BrickPi.MotorSpeed[RWHEEL] = WHEELPOWER
+		
 	
 	#check object detection
-	if usreading < USSTANDARD:
+	if takeusreading() < USSTANDARD:
+		
 		#slide up
 		movelimb(ARM, -30, 0.3)
+		if takeusreading() > USSTANDARD:
+			#low-lying object
 		
-		
-		#object detected
-		print "object detected"
-		print "stopping"
-		BrickPi.MotorSpeed[LWHEEL] = 0
-		BrickPi.MotorSpeed[RWHEEL] = 0
-		#shooby forward a wee
-#		movelimb(LWHEEL, WHEELPOWER, 0.7)
-#		movelimb(RWHEEL, WHEELPOWER, 0.7)
-#		BrickPi.MotorSpeed[LWHEEL] = 0
-#		BrickPi.MotorSpeed[RWHEEL] = 0
-		
-		time.sleep(1)
-		print "sliding down"
-		movelimb(ARM, SLIDEDOWNPOWER, 0.3)
-		
-#		print "closing"
-#		movelimb(GRABBER, GRABBERPOWER, 0.5)
-#		time.sleep(0.5)
-		
-		print "lifting"
-		movelimb(ARM, LIFTPOWER, 0.7, 1) #grabber grips as well
+			print "object detected"
+			print "stopping"
+			BrickPi.MotorSpeed[LWHEEL] = 0
+			BrickPi.MotorSpeed[RWHEEL] = 0
+			#shooby forward a wee
+	#		movelimb(LWHEEL, WHEELPOWER, 0.7)
+	#		movelimb(RWHEEL, WHEELPOWER, 0.7)
+	#		BrickPi.MotorSpeed[LWHEEL] = 0
+	#		BrickPi.MotorSpeed[RWHEEL] = 0
+			
+			time.sleep(1)
+			print "sliding down"
+			movelimb(ARM, SLIDEDOWNPOWER, 0.3)
+			
+			print "lifting"
+			movelimb(ARM, LIFTPOWER, 0.7, 1) #grabber grips as well
 
-		print "opening"
-		movelimb(GRABBER, OPENPOWER, 0.3)
+			print "opening"
+			movelimb(GRABBER, OPENPOWER, 0.3)
 
-		print "bringing down"
-		movelimb(ARM, BRINGDOWNPOWER, 0.3)
-		time.sleep(0.5)
-
-#		print "sliding down"
-#		movelimb(ARM, SLIDEDOWNPOWER, 0.3)
-		
-		
-		BrickPi.MotorSpeed[GRABBER] = 0
-		BrickPi.MotorSpeed[ARM] = 0		
-		time.sleep(2)
+			print "bringing down"
+			movelimb(ARM, BRINGDOWNPOWER, 0.3)
+			time.sleep(0.5)
+			
+			#stop movement
+			BrickPi.MotorSpeed[GRABBER] = 0
+			BrickPi.MotorSpeed[ARM] = 0		
+			time.sleep(2)
+		else:
+			#wall
+			pass
 
