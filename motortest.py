4e211e2f6556e6669788e29340a68482a4b31bcb
Andrewwango <andrew.wang27@gmail.com>
2016-05-07 13:55:19 +0100

    Update motortest.py

--- motortest.py
+++ motortest.py
@@ -7,10 +7,10 @@ BrickPi.MotorEnable[PORT_C] = 1
 BrickPiSetupSensors()
 
 
-while True:
-    BrickPi.MotorSpeed[PORT_C] = -100
+BrickPi.MotorSpeed[PORT_C] = -200
 
-    ot = time.time()
-    while(time.time() - ot < 1):    #running while loop for 3 seconds
-        BrickPiUpdateValues()       # Ask BrickPi to update values for sensors/motors
+ot = time.time()
+while(time.time() - ot < 1):    #running while loop for 3 seconds
+	BrickPiUpdateValues()       # Ask BrickPi to update values for sensors/motors
 	time.sleep(.1)
+
