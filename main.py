00082eb431a58b8bda7fca2841fb44b832c1dd71
Andrewwango <andrew.wang27@gmail.com>
2016-05-08 16:28:28 +0100

    Update main.py

--- main.py
+++ main.py
@@ -117,7 +117,7 @@ while True:
 			
 			#reverse, turn right, go, then turn right again
 			movelimb(LWHEEL, -WHEELPOWER, 0.4, RWHEEL, -WHEELPOWER)
-			movelimb(LWHEEL,  TURNPOWER, 1, RWHEEL, -TURNPOWER)			
+			movelimb(LWHEEL,  TURNPOWER, 1.4, RWHEEL, -TURNPOWER)			
 			movelimb(LWHEEL,  WHEELPOWER, 0.4, RWHEEL,  WHEELPOWER)
-			movelimb(LWHEEL,  TURNPOWER, 1, RWHEEL, -TURNPOWER)
+			movelimb(LWHEEL,  TURNPOWER, 1.4, RWHEEL, -TURNPOWER)
 			drivewheels(0,0)	
