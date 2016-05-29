c9b8b9e73c014624f32b8c965f096a4e6a461390
Andrewwango <andrew.wang27@gmail.com>
2016-05-29 14:24:39 +0200

    Create gpiotest.py

---
+++ gpiotest.py
@@ -0,0 +1,21 @@
+import RPI.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(25, GPIO.IN) #yellow
+GPIO.setup(24, GPIO.OUT) #brown
+
+GPIO.output(24, False)
+time.sleep(0.1)
+GPIO.output(24, True)
+time.sleep(0.001)
+GPIO.output(24, False)
+
+start = time.time()
+stop = time.time()
+while GPIO.input(25) == 0:
+	start = time.time()
+while GPIO.input(25) == 1:
+	stop = time.time()
+duration = stop - start
+distance = duration * 340 * 100 #cm
