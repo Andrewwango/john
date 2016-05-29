ab0f1bc3759bfedd9391c9977e996152b326fad2
Andrewwango <andrew.wang27@gmail.com>
2016-05-29 14:28:48 +0200

    Update gpiotest.py

--- gpiotest.py
+++ gpiotest.py
@@ -5,18 +5,20 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(25, GPIO.IN) #yellow
 GPIO.setup(24, GPIO.OUT) #brown
 
-GPIO.output(24, False)
-time.sleep(0.1)
-GPIO.output(24, True)
-time.sleep(0.001)
-GPIO.output(24, False)
-
-start = time.time()
-stop = time.time()
-while GPIO.input(25) == 0:
+while True:
+	GPIO.output(24, False)
+	time.sleep(0.1)
+	GPIO.output(24, True)
+	time.sleep(0.001)
+	GPIO.output(24, False)
+	
 	start = time.time()
-while GPIO.input(25) == 1:
 	stop = time.time()
-duration = stop - start
-distance = duration * 340 * 100 #cm
-print distance
+	while GPIO.input(25) == 0:
+		start = time.time()
+	while GPIO.input(25) == 1:
+		stop = time.time()
+	duration = stop - start
+	distance = duration * 340 * 100 #cm
+	print distance
+	time.sleep(0.5)
