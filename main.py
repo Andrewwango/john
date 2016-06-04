5e62001d2174960b052741ae4d6fb5fa747ba8d3
Andrewwango <andrew.wang27@gmail.com>
2016-06-04 14:19:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
-from BrickPi import *
+import BrickPi
 import time
-BrickPiSetup()
+import RPi.GPIO as GPIO
+BrickPi.BrickPiSetup()
+GPIO.setmode(GPIO.BCM)
 
 ##DEBUG##
 #if motors stalling - check voltage + PSU
-#brickpiupdatevalues error - do system thingy
+#BrickPi.BrickPiUpdateValues error - do system thingy (./stopev.sh)
 #if result=-1 - reboot
 
 ##NOTES##
-#default position is opened grabber, half down	
+#default position is grabber open, arm fully up
 #positive speed = rolling away from bum
 
 ##CONSTANTS##
@@ -17,27 +19,37 @@ RWHEEL = PORT_A
 GRABBER = PORT_B
 ARM = PORT_C
 HEAD = PORT_1
-USSTANDARD = 25 #us sensor threshold
-WHEELPOWER     = -100
-GRABBERPOWER   =  -100
-LIFTPOWER      = -230
-SLIDEDOWNPOWER =  60
-OPENPOWER      = 40
-BRINGDOWNPOWER =  100
-SLIDEUPPOWER = -60
-TURNPOWER = -255
 
+IRIN = 25 #yellow (when sth close, 0)
+USTRIG = 24 #brown, out
+USECHO = 23 #green, in
+
+USSTANDARD     = 25 #us sensor threshold
+US2STANDARD    = 100
+
+WHEELPOWER     = -100
+TURNPOWER      = -255
+
+GRABBERPOWER   = -100
+OPENPOWER      = 40
+
+LIFTPOWER      = -230
+BRINGDOWNPOWER = 100
 
 ##SETUP##
 BrickPi.MotorEnable[GRABBER] = 1
 BrickPi.MotorEnable[ARM] = 1
 BrickPi.MotorEnable[LWHEEL] = 1
 BrickPi.MotorEnable[RWHEEL] = 1
-BrickPi.SensorType[HEAD] = TYPE_SENSOR_ULTRASONIC_CONT
-BrickPiSetupSensors()
+GPIO.setup(IRIN, GPIO.IN)
+GPIO.setup(USECHO, GPIO.IN)
+GPIO.setup(USTRIG, GPIO.OUT)
+BrickPi.SensorType[HEAD] = BrickPi.TYPE_SENSOR_ULTRASONIC_CONT
+BrickPi.BrickPiSetupSensors()
 
-
+#############
 ##FUNCTIONS##
+#############
 def movelimb(limb, speed, length, limb2=None, speed2=None):
 	#set speeds
 	BrickPi.MotorSpeed[limb] = speed
@@ -45,82 +57,102 @@ def movelimb(limb, speed, length, limb2=None, speed2=None):
 		BrickPi.MotorSpeed[limb2] = speed2
 	ot = time.time()
 	while(time.time() - ot < length):
-		BrickPiUpdateValues()
+		BrickPi.BrickPiUpdateValues()
 	time.sleep(.1)
 
 def takeusreading():
-	#take 7 readings then find mode
+	#take 9 readings then find mode
 	uslist=[]
-	BrickPi.MotorSpeed[GRABBER] = OPENPOWER #make sure grabber dunt interfere with reading
 	for i in range(9):
-		result = BrickPiUpdateValues()
-		#print result
+		result = BrickPi.BrickPiUpdateValues()
 		if not result:
 			uslist += [int(BrickPi.Sensor[HEAD])]
 		time.sleep(.05)
 	print uslist
-	BrickPi.MotorSpeed[GRABBER] = 0
-	#usreading = max(set(uslist), key=uslist.count) #mode
-	usreading = sum(uslist)/len(uslist) #mean
+	usreading = max(set(uslist), key=uslist.count) #mode
+	#usreading = sum(uslist)/len(uslist) #mean
 	print "usreading is " + str(usreading)
 	return usreading
+	
+def takeus2reading():
+	#take 5 readings then find mode
+	us2list=[]
+	for i in range(5):
+		#send out signal
+		GPIO.output(USTRIG, False)
+		time.sleep(0.1)
+		GPIO.output(USTRIG, True)
+		time.sleep(0.001)
+		GPIO.output(USTRIG, False)
+		
+		#find length of signal
+		start = time.time()
+		stop = time.time()
+		while GPIO.input(USECHO) == 0:
+			start = time.time()
+		while GPIO.input(USECHO) == 1:
+			stop = time.time()
+		duration = stop - start
+		
+		#find length
+		distance = duration * 340 * 100 #cm
+		uslist += [int(distance)]
+		time.sleep(0.1)
+	print us2list
+	us2reading = max(set(us2list), key=us2list.count) #mode
+	print "higher us2reading is " + str(us2reading)
+	return us2reading
 
 def drivewheels(lpower, rpower):
 	BrickPi.MotorSpeed[LWHEEL] = lpower
 	BrickPi.MotorSpeed[RWHEEL] = rpower	
 
+#############
 ##MAIN LOOP##
+#############
 while True:
 	#stop actions
 	BrickPi.MotorSpeed[GRABBER] = 0
 	BrickPi.MotorSpeed[ARM] = 0
+	
 	#drive
 	drivewheels(WHEELPOWER, WHEELPOWER)
-		
 	
-	#check object detection
+	#check us for object
 	if takeusreading() < USSTANDARD:
 		print "object detected"
 		
-		#stop and slide up to check
-		print "stopping and checking"
-		drivewheels(0,0)
-		movelimb(ARM, SLIDEUPPOWER, 0.2)
-		time.sleep(0.2)
+		#check higher us2 for big thing
+		print "sliding down" #activate us2 pos
+		movelimb(ARM, BRINGDOWNPOWER, 0.1)
 		
-		if takeusreading() > USSTANDARD:
-			#low-lying object
-		
+		if takeus2reading() > US2STANDARD:
+			#low-lying object - pick up litter procedure
 			print "low-lying object detected"
-			
 			time.sleep(1)
-			print "sliding down"
-			movelimb(ARM, SLIDEDOWNPOWER, 0.5)
-			time.sleep(0.2)
-			
-			#shooby forward a wee
-			print "shooby"
+
+			print "shooby" #shooby forwards to get into place
 			movelimb(LWHEEL,  WHEELPOWER, 0.3, RWHEEL,  WHEELPOWER)
-			drivewheels(0,0)			
-			
+			drivewheels(0,0)
+		
+			print "bringing down" #get grabber into pos
+			movelimb(ARM, BRINGDOWNPOWER, 0.5)
+			time.sleep(0.2)
+
 			#preliminary grab
 			movelimb(GRABBER, GRABBERPOWER, 0.4)
 			
-			print "lifting"
+			print "lifting" #bring litter up
 			movelimb(ARM, LIFTPOWER, 0.7, GRABBER, GRABBERPOWER) #grabber grips as well
 
-			print "opening"
+			print "opening" #dump litter
 			movelimb(GRABBER, OPENPOWER, 0.5)
-
-			print "bringing down"
-			movelimb(ARM, BRINGDOWNPOWER, 0.4)
-			time.sleep(0.5)
-			
 			time.sleep(2)
+			
 		else:
 			#wall
-			print "wall: sliding down"
-			movelimb(ARM, SLIDEDOWNPOWER, 0.3)
+			print "wall: sliding up" #get back into normal pos
+			movelimb(ARM, LIFTPOWER, 0.1)
 			time.sleep(1)
 			
 			#reverse, turn right, go, then turn right again
@@ -128,4 +160,8 @@ while True:
 			movelimb(LWHEEL,  TURNPOWER, 2, RWHEEL, -TURNPOWER)			
 			movelimb(LWHEEL,  WHEELPOWER, 0.4, RWHEEL,  WHEELPOWER)
 			movelimb(LWHEEL,  TURNPOWER, 2, RWHEEL, -TURNPOWER)
-			drivewheels(0,0)	
+			drivewheels(0,0)
+	
+	#check IR for cliff
+	if GPIO.input(IRIN) == 1: #nothing close
+		pass
