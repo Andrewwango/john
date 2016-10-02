7cf4b9de37e0cfcf16b449416396759dc5f07d02
Andrewwango <andrew.wang27@gmail.com>
2016-10-02 21:11:56 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-####SORT OUT DISTANCE ADJUSTING WHEN DETECTED LITTER###
+####TURNING PROGRAMMING
+####best collection of data from us and us2 (mode/mean/repeats)
 from BrickPi import *
 import time
 import RPi.GPIO as GPIO
@@ -18,41 +19,42 @@ GPIO.setmode(GPIO.BCM)
 #encoder increase = rolling away from bum
 
 '''PROGRAM
- XX- turn x degrees.
+- start facing 90o right
+- turn x degrees left
 - go until edge
-- turn -2x degrees and continue, turning -2x degrees each time
+- turn 2x degrees and continue, turning 2x degrees each time
 - measure degrees using outside wheel
 '''
 
 ##CONSTANTS##
+#Port Assignments
 LWHEEL = PORT_D
 RWHEEL = PORT_A
 GRABBER = PORT_B
 ARM = PORT_C
 HEAD = PORT_1
 
+#GPIO Pins
 IRIN = 25 #yellow (when sth close, 0)
 USTRIG = 24 #brown, out
 USECHO = 23 #green, in
 
-XDEGREES=200
+XDEGREES=200 #angle between robot path and normal to edge, measured by outside wheel (in encoderdegs)
 
-USSTANDARD     = 25 #us sensor threshold
-US2STANDARD    = 100
+USSTANDARD     = 25 #us sensor detection threshold
+US2STANDARD    = 100 #higher us(2) detection threshold
 MINLITTERRANGE = 10 #the min distance from which it can pick up stuff
 
 WHEELPOWER     = -255
 TURNPOWER      = 255
 SHOOBYPOWER    = -100
-
 GRABBERPOWER   = -100
 OPENPOWER      = 80
-
 LIFTPOWER      = -200
 BRINGDOWNPOWER = 100
 BRINGDOWNBRAKEPOWER = -5
 
-##SETUP##
+##SETUP## motors, sensors, GPIO Pins
 BrickPi.MotorEnable[GRABBER] = 1
 BrickPi.MotorEnable[ARM] = 1
 BrickPi.MotorEnable[LWHEEL] = 1
@@ -68,21 +70,21 @@ turnycount = 0
 #############
 ##FUNCTIONS##
 #############
-def takeusreading():
-	#take 9 readings then find mode
+def takeusreading(): #detect distance of us
+	#take 9 readings then find average
 	uslist=[]
 	for i in range(9):
 		result = BrickPiUpdateValues()
 		if not result:
 			uslist += [int(BrickPi.Sensor[HEAD])]
-		time.sleep(.05)
+		time.sleep(.02)
 	print uslist
-	usreading = max(set(uslist), key=uslist.count) #mode
+	usreading = max(set(uslist), key=uslist.count) #mode (removes anomalies)
 	#usreading = sum(uslist)/len(uslist) #mean
 	print "usreading is " + str(usreading)
 	return usreading
 	
-def takeus2reading():
+def takeus2reading(): #detect distance of us2
 	#take 5 readings then find mode
 	us2list=[]
 	for i in range(5):
@@ -112,7 +114,7 @@ def takeus2reading():
 	print "higher us2reading is " + str(us2reading)
 	return us2reading
 
-def takeencoderreading(port):
+def takeencoderreading(port): #read motor position
 	result = BrickPiUpdateValues()
 	if not result :
 		print "encoder reading is: " + str((BrickPi.Encoder[port]) /2)
@@ -133,14 +135,13 @@ def turnprocedure(thecountvar):
 	else:
 		print "turning right" #use left wheel to encode
 		movelimbENC(LWHEEL, TURNPOWER, XDEGREES*2, RWHEEL, -TURNPOWER)
-	drivewheels(0,0)
 	thecountvar += 1 #next time turns other way
 	time.sleep(0.5)	
 
-def movelimbLENG(limb, speed, length, limb2=None, speed2=None): #movelimb based on time length
+def movelimbLENG(limb, speed, length, limb2=None, speed2=None): #move motor based on time length
 	#set speeds
 	BrickPi.MotorSpeed[limb] = speed
-	if limb2 != None:
+	if limb2 != None: #optional simultaneous second motor movement
 		BrickPi.MotorSpeed[limb2] = speed2
 	ot = time.time()
 	while(time.time() - ot < length):
@@ -151,8 +152,8 @@ def movelimbLENG(limb, speed, length, limb2=None, speed2=None): #movelimb based 
 	if limb2 != None:
 		BrickPi.MotorSpeed[limb2] = 0
 
-def movelimbENC(limb, speed, encoderdeg, limb2=None, speed2=None): #movelimb based on encoderdeg
-	#encoderdeg is the change in encoder degrees
+def movelimbENC(limb, speed, encoderdeg, limb2=None, speed2=None): #move motor based on encoder
+	#encoderdeg is the change in encoder degrees (scalar)
 	#positive speed is positive encoder increase
 	#i love to wear blue, red and pink dungarees
 	startpos = takeencoderreading(limb)
@@ -160,13 +161,13 @@ def movelimbENC(limb, speed, encoderdeg, limb2=None, speed2=None): #movelimb bas
 		while takeencoderreading(limb) - startpos < encoderdeg:
 			#carry on turning till arm reaches correct pos
 			BrickPi.MotorSpeed[limb] = speed
-			if limb2 != None:
+			if limb2 != None: #optional simultaneous second motor movement
 				BrickPi.MotorSpeed[limb2] = speed2
 	elif speed < 0:
 		while takeencoderreading(limb) - startpos > -encoderdeg:
 			#carry on turning till arm reaches correct pos
 			BrickPi.MotorSpeed[limb] = speed
-			if limb2 != None:
+			if limb2 != None: #optional simultaneous second motor movement
 				BrickPi.MotorSpeed[limb2] = speed2
 	#stop
 	BrickPi.MotorSpeed[limb] = 0
@@ -177,8 +178,8 @@ def movelimbENC(limb, speed, encoderdeg, limb2=None, speed2=None): #movelimb bas
 ################
 ##MAIN PROGRAM##
 ################
-#1. turn x degrees RIGHT to start
-#turnprocedure(turnycount)
+#1. turn x degrees LEFT to reach right facing direction
+#movelimbENC(RWHEEL, -TURNPOWER, XDEGREES, LWHEEL, TURNPOWER) #intial turn from facing 90o right
 
 #main loop
 while True:
@@ -201,13 +202,12 @@ while True:
 		movelimbLENG(ARM, BRINGDOWNBRAKEPOWER, 0.1) #brake to prevent coast
 		time.sleep(0.5)
 
-
 		#check higher us2 for big thing		
 		if takeus2reading() > US2STANDARD:
 			#LITTER (low-lying object)
 			#pick up litter procedure
 			print "low-lying object detected"
-			time.sleep(1)
+			time.sleep(0.5)
 
 			if tempreading <= MINLITTERRANGE: #too close
 				print "too close, shoobying AWAY"
@@ -237,7 +237,7 @@ while True:
 			#loop back and carry on
 	
 	#check IR for cliff
-	if GPIO.input(IRIN) == 1: #nothing close
+	if GPIO.input(IRIN) == 1: #nothing close (underneath sensor)
 		#CLIFF
 		print "CLIFF"
 		turnprocedure(turnycount)
