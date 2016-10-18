c07a3c90eff91cd1297750e93032d00e2319d711
Andrewwango <andrew.wang27@gmail.com>
2016-10-18 20:46:54 +0100

    Update encodertest.py

--- encodertest.py
+++ encodertest.py
@@ -1,13 +1,15 @@
 from BrickPi import *
 BrickPiSetup()
 
-BrickPi.MotorEnable[PORT_C] = 1
+BrickPi.MotorEnable[PORT_A] = 1
 BrickPiSetupSensors()
+
+result = BrickPiUpdateValues()
+if not result :
+    initialr = BrickPi.Encoder[PORT_A]
 
 while True:
     result = BrickPiUpdateValues()
     if not result :
-        print ( BrickPi.Encoder[PORT_C] ) /2
+        print BrickPi.Encoder[PORT_A]-initialr
     time.sleep(.4)
-
-# Note: One encoder value counts for 0.5 degrees. So 360 degrees = 720 enc. Hence, to get degress = (enc%720)/2
