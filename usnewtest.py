cbfb051f19a3f7e05b49afaf2be0cbfc4ee456a4
Andrewwango <andrew.wang27@gmail.com>
2016-11-12 21:48:31 +0000

    Update usnewtest.py

--- usnewtest.py
+++ usnewtest.py
@@ -8,19 +8,22 @@ GPIO.setup(24, GPIO.OUT) #brown, trig
 GPIO.output(24, False); GPIO.output(17, False)
 
 while True:
-	GPIO.output(17, False)
-	time.sleep(0.1)
-	GPIO.output(17, True)
-	time.sleep(0.001)
-	GPIO.output(17, False)
-	
-	start = time.time()
-	stop = time.time()
-	while GPIO.input(22) == 0:
+	try:
+		GPIO.output(17, False)
+		time.sleep(0.1)
+		GPIO.output(17, True)
+		time.sleep(0.001)
+		GPIO.output(17, False)
+
 		start = time.time()
-	while GPIO.input(22) == 1:
 		stop = time.time()
-	duration = stop - start
-	distance = int(duration * 340 * 100) #cm
-	print distance
-	time.sleep(0.5)
+		while GPIO.input(22) == 0:
+			start = time.time()
+		while GPIO.input(22) == 1:
+			stop = time.time()
+		duration = stop - start
+		distance = int(duration * 340 * 100) #cm
+		print distance
+		time.sleep(0.5)
+	except KeyboardInterrupt:
+		GPIO.cleanup()
