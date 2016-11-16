ac16d4a27905e02d7d61168e781c64008406b7ae
Andrewwango <andrew.wang27@gmail.com>
2016-11-16 20:04:37 +0000

    Update byebye.py

--- byebye.py
+++ byebye.py
@@ -2,16 +2,21 @@ from BrickPi import *
 import time
 
 BrickPiSetup()
-BrickPi.MotorEnable[PORT_A] = 1
-BrickPi.MotorEnable[PORT_D] = 1
-
+BrickPi.MotorEnable[PORT_C] = 1
 BrickPiSetupSensors()
 
-
-BrickPi.MotorSpeed[PORT_D] = -200
-BrickPi.MotorSpeed[PORT_A] = -200
-ot = time.time()
-while(time.time() - ot < 5):    #running while loop for 3 seconds
-	BrickPiUpdateValues()       # Ask BrickPi to update values for sensors/motors
-	time.sleep(.1)
+for i in range(5):
+	#down
+	BrickPi.MotorSpeed[PORT_C] = 120
+	ot = time.time()
+	while(time.time() - ot < 0.5):
+		BrickPiUpdateValues()
+		time.sleep(.1)
+	#up
+	BrickPi.MotorSpeed[PORT_C] = -150
+	ot = time.time()
+	while(time.time() - ot < 0.5):
+		BrickPiUpdateValues()
+		time.sleep(.1)		
+		
 
