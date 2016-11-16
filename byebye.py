6a477a9a932d0a010e4bad3abd729e48bcbb7ba8
Andrewwango <andrew.wang27@gmail.com>
2016-11-16 20:33:42 +0000

    Update byebye.py

--- byebye.py
+++ byebye.py
@@ -8,22 +8,22 @@ BrickPiSetupSensors()
 #down
 BrickPi.MotorSpeed[PORT_C] = 80
 ot = time.time()
-while(time.time() - ot < 0.3):
+while(time.time() - ot < 0.5):
 	BrickPiUpdateValues()
 	time.sleep(.1)
 
 for i in range(5):
 
 	#up
-	BrickPi.MotorSpeed[PORT_C] = -100
+	BrickPi.MotorSpeed[PORT_C] = -80
 	ot = time.time()
-	while(time.time() - ot < 0.2):
+	while(time.time() - ot < 0.3):
 		BrickPiUpdateValues()
 		time.sleep(.1)		
 		
 	#down
 	BrickPi.MotorSpeed[PORT_C] = 80
 	ot = time.time()
-	while(time.time() - ot < 0.2):
+	while(time.time() - ot < 0.4):
 		BrickPiUpdateValues()
 		time.sleep(.1)
