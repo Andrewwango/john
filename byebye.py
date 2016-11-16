c6f9c14f35920c474b88b271397757e99af5aa26
Andrewwango <andrew.wang27@gmail.com>
2016-11-16 20:01:42 +0000

    Create byebye.py

---
+++ byebye.py
@@ -0,0 +1,17 @@
+from BrickPi import *
+import time
+
+BrickPiSetup()
+BrickPi.MotorEnable[PORT_A] = 1
+BrickPi.MotorEnable[PORT_D] = 1
+
+BrickPiSetupSensors()
+
+
+BrickPi.MotorSpeed[PORT_D] = -200
+BrickPi.MotorSpeed[PORT_A] = -200
+ot = time.time()
+while(time.time() - ot < 5):    #running while loop for 3 seconds
+	BrickPiUpdateValues()       # Ask BrickPi to update values for sensors/motors
+	time.sleep(.1)
+
