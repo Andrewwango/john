8224fe4b6b23d9ce2d015cc0af1bca1bf18ee518
Andrewwango <andrew.wang27@gmail.com>
2017-01-10 20:46:40 +0000

    Update motortest.py

--- motortest.py
+++ motortest.py
@@ -2,13 +2,12 @@ import time
 from BrickPi import *
 
 BrickPiSetup()
-BrickPi.MotorEnable[PORT_B] = 1
+BrickPi.MotorEnable[PORT_D] = 1
 #BrickPi.MotorEnable[PORT_D] = 1
-
 BrickPiSetupSensors()
 
 
-BrickPi.MotorSpeed[PORT_B] = -200
+BrickPi.MotorSpeed[PORT_D] = -200
 #BrickPi.MotorSpeed[PORT_A] = -200
 ot = time.time()
 while(time.time() - ot < 5):    #running while loop for 3 seconds
