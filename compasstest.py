8c5e58473169dfa5f95ddcc8b3d2a0fdf1f79d07
Andrewwango <andrew.wang27@gmail.com>
2017-01-17 22:18:38 +0000

    Update compasstest.py

--- compasstest.py
+++ compasstest.py
@@ -32,36 +32,14 @@ write_byte(2, 0b00000000) # Continuous sampling
 
 scale = 0.92
 
-for i in range(0,500):
-    minx = 0
-    maxx = 0
-    miny = 0
-    maxy = 0
+x_offset = -23
+y_offset = -84
+x_out = (read_word_2c(3) - x_offset) * scale
+y_out = (read_word_2c(7) - y_offset) * scale
+z_out = (read_word_2c(5)) * scale
 
-    for i in range(0,500):
-        x_out = read_word_2c(3)
-        y_out = read_word_2c(7)
-        z_out = read_word_2c(5)
+bearing  = math.atan2(y_out, x_out) 
+if (bearing < 0):
+    bearing += 2 * math.pi
 
-
-        if x_out < minx:
-            minx=x_out
-
-        if y_out < miny:
-            miny=y_out
-
-        if x_out > maxx:
-            maxx=x_out
-
-        if y_out > maxy:
-            maxy=y_out
-
-        #print x_out, y_out, (x_out * scale), (y_out * scale)
-        time.sleep(0.1)
-
-    print "minx: ", minx
-    print "miny: ", miny
-    print "maxx: ", maxx
-    print "maxy: ", maxy
-    print "x offset: ", (maxx + minx) / 2
-    print "y offset: ", (maxy + miny) / 2
+print "Bearing: ", math.degrees(bearing)
