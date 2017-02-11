f532c23eff83413e7e5ef1dac144a662e962b74d
Andrewwango <andrew.wang27@gmail.com>
2017-02-11 14:57:57 +0000

    Update compasscalib2.py

--- testers/compasscalib2.py
+++ testers/compasscalib2.py
@@ -33,13 +33,15 @@ write_byte(2, 0b00000000) # Continuous sampling
 scale = 0.92
 
 for i in range(0,500):
-    x_out = read_word_2c(3)
-    y_out = read_word_2c(7)
-    z_out = read_word_2c(5)
+    x_offset = -10
+    y_offset = 10
+    x_out = (read_word_2c(3) - x_offset) * scale
+    y_out = (read_word_2c(7) - y_offset) * scale
+    z_out = (read_word_2c(5)) * scale
     
     bearing  = math.atan2(y_out, x_out) 
     if (bearing < 0):
         bearing += 2 * math.pi
     
-    print x_out, y_out, (x_out * scale), (y_out * scale)
+    print x_out * scale, y_out * scale
     time.sleep(0.1)
