99229ca5e2b9158ab188208e9d1980e184c1aab8
Andrewwango <andrew.wang27@gmail.com>
2017-02-11 17:33:51 +0000

    Update encodertest.py

--- testers/encodertest.py
+++ testers/encodertest.py
@@ -1,15 +1,15 @@
 from BrickPi import *
 BrickPiSetup()
-
-BrickPi.MotorEnable[PORT_D] = 1
+port=PORT_D
+BrickPi.MotorEnable[port] = 1
 BrickPiSetupSensors()
 
 result = BrickPiUpdateValues()
 if not result :
-    initialr = BrickPi.Encoder[PORT_A]
+    initialr = BrickPi.Encoder[port]
 
 while True:
     result = BrickPiUpdateValues()
     if not result :
-        print BrickPi.Encoder[PORT_A]-initialr
+        print BrickPi.Encoder[port]-initialr
     time.sleep(.4)
