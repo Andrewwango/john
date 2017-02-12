40f57a07334ea33b41e8033dd3e20ebdeb5bf97c
Andrewwango <andrew.wang27@gmail.com>
2017-02-12 14:22:34 +0000

    Update cmpautocalib.py

--- cmpautocalib.py
+++ cmpautocalib.py
@@ -1,7 +1,7 @@
 import time
 from BrickPi import *
 
-SPEED=110 #pos acw
+SPEED=110 #pos acw first, cw 2nd
 
 BrickPiSetup()
 BrickPi.MotorEnable[PORT_A]=1
@@ -15,15 +15,19 @@ while True: #make sure we get an encoder reading! (break when we do)
 		initialr = BrickPi.Encoder[PORT_A]
 		break
 
-BrickPi.MotorSpeed[PORT_A]=-SPEED
-BrickPi.MotorSpeed[PORT_D]=SPEED
-while True:
-	result = BrickPiUpdateValues()
-	if not result :
-		encr = BrickPi.Encoder[PORT_A]-initialr
-		print encr
-		if abs(encr) > 1770: #a full turn
-			break #finshed turning
-	time.sleep(.1)
+for i in range(2):
+	if i==0: BrickPi.MotorSpeed[PORT_A]=-SPEED; BrickPi.MotorSpeed[PORT_D]= SPEED 
+	else:    BrickPi.MotorSpeed[PORT_A]= SPEED; BrickPi.MotorSpeed[PORT_D]=-SPEED #turn back
 
+	while True:
+		result = BrickPiUpdateValues()
+		if not result :
+			encr = BrickPi.Encoder[PORT_A]-initialr
+			print encr
+			if abs(encr) > 1770: #a full turn
+				break #finshed turning
+		time.sleep(.1)
+
+	BrickPi.MotorSpeed[PORT_A]=0; BrickPi.MotorSpeed[PORT_D]=0
+	
 #process results
