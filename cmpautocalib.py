ceb3511a5a0666d9e0352af9bee2cf4a293b1508
Andrewwango <andrew.wang27@gmail.com>
2017-02-12 14:07:41 +0000

    Update cmpautocalib.py

--- cmpautocalib.py
+++ cmpautocalib.py
@@ -1,1 +1,16 @@
+import time
+from BrickPi import *
 
+
+BrickPiSetup()
+BrickPi.MotorEnable[PORT_A]=1
+BrickPi.MotorEnable[PORT_D]=1
+BrickPiSetupSensors()
+
+
+BrickPi.MotorSpeed[PORT_A]=-70
+BrickPi.MotorSpeed[PORT_D]=70
+ot = time.time()
+while(time.time() - ot < 2):    #running while loop for 3 seconds
+	BrickPiUpdateValues()       # Ask BrickPi to update values for sensors/motors
+	time.sleep(.1)
