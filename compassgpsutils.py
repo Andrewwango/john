6abef13629ea45bc358c575e7289b4119f585439
Andrewwango <andrew.wang27@gmail.com>
2017-02-12 16:19:02 +0000

    Update compassgpsutils.py

--- compassgpsutils.py
+++ compassgpsutils.py
@@ -8,6 +8,8 @@ scale=0.92
 settingsfile=open("mainsettings.dat","r"); settings=settingsfile.read().split("\n")
 x_offset = int(settings[0])
 y_offset = int(settings[1])
+print settings
+print x_offset, y_offset
 settingsfile.close()
 
 #gps setup
@@ -41,7 +43,7 @@ write_byte(2, 0b00000000) # Continuous sampling
 def takebearing():
 	x_out = (read_word_2c(3) - x_offset) * scale
 	y_out = (read_word_2c(7) - y_offset) * scale
-
+	print x_out, y_out
 	bearing  = math.atan2(y_out, x_out) 
 	if (bearing < 0):
 		bearing += 2 * math.pi
