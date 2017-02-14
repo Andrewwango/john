a0793fc8cbbb3bbbb7e9813b6d47dbdc39622ee9
Andrewwango <andrew.wang27@gmail.com>
2017-02-14 12:29:26 +0000

    Update irrctest.py

--- testers/irrctest.py
+++ testers/irrctest.py
@@ -2,8 +2,10 @@ import lirc, time
 
 sockid = lirc.init("irrctest",blocking=False)
 print("Ready")
-
+i=0
 while True:
+	i+=1
+	print i
 	code = lirc.nextcode()
 	if code: print(code[0])
-	time.sleep(0.2)
+	time.sleep(0.5)
