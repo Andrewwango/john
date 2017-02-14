0aef95c54549defe37823ab4ba984c90f95a3449
Andrewwango <andrew.wang27@gmail.com>
2017-02-14 16:09:59 +0000

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,9 @@
-import BrickPi
-print "imported"
+import os, sys, time
+i=0
 
-BrickPi.BrickPiSetup()
-print "setup"
+while 1:
+	print i
+	i+=1
+	time.sleep(0.5)
+	if i==10:
+		os.execl(sys.executable, sys.executable, *sys.argv)
