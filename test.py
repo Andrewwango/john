bd868be19d4f3ef24280bd1ceea01620286e9158
Andrewwango <andrew.wang27@gmail.com>
2017-02-14 19:07:36 +0000

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,3 @@
-print "SATATR"
+import cmpautocalib
 
-import os, sys, time
-i=0
-
-while 1:
-	print i
-	i+=1
-	time.sleep(0.5)
-	if i==10:
-		os.execl(sys.executable, sys.executable, *sys.argv)
+cmpautocalib.maincalibprogram()
