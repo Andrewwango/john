668857fcf39829e205ca6b11097fc9408380fb45
Andrewwango <andrew.wang27@gmail.com>
2017-02-18 21:41:12 +0000

    Update touch2test.py

--- testers/touch2test.py
+++ testers/touch2test.py
@@ -2,19 +2,19 @@ import time
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(11, GPIO.IN)
-GPIO.setup(4, GPIO.IN)
+GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 while True:
 	state=GPIO.input(11)
 	if state==0:
-		print "0"
+		print "11111111111111111111111111111111111111111111111"
 	elif state==1:
-		print "1111111111111111111111111111111111"
+		print "1"
 	state2=GPIO.input(4)
  	if state==0:
-		print "0"
+		print "222222222222222222222222222222222222222222222"
 	elif state==1:
-		print "222222222222222222222222222222222"
-   
+		print "2"
+		
 	time.sleep(0.2)
