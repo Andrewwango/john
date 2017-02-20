d1b1a2c87c2064c247c1c97e7d32bacde8be05ee
Andrewwango <andrew.wang27@gmail.com>
2017-02-20 17:04:47 +0000

    Update touch2test.py

--- testers/touch2test.py
+++ testers/touch2test.py
@@ -6,15 +6,6 @@ GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 while True:
-	state=GPIO.input(11)
-	if state==0:
-		print "11111111111111111111111111111111111111111111111"
-	elif state==1:
-		print "1"
-	state2=GPIO.input(4)
- 	if state2==0:
-		print "222222222222222222222222222222222222222222222"
-	elif state2==1:
-		print "2"
+	print GPIO.input(11), GPIO.input(4)
 		
 	time.sleep(0.2)
