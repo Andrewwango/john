2d6a89cef4da31b1e1be3e8d55b33261d16a6c71
Andrewwango <andrew.wang27@gmail.com>
2017-02-20 19:23:44 +0000

    Update pishutdown.py

--- pishutdown.py
+++ pishutdown.py
@@ -5,12 +5,13 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 
 SHUTBUTT = 9
+INDICLED = 27
 GPIO.setup(SHUTBUTT, GPIO.IN , pull_up_down=GPIO.PUD_UP)
+GPIO.setup(INDICLED, GPIO.OUT)
 
 GPIO.wait_for_edge(SHUTBUTT, GPIO.FALLING)
 
 print "SHUTBUTT PRESSED, SHUTTING DOWN!!"
-
-GPIO.cleanup()
+GPIO.output(INDICLED, True)
 
 os.system('sudo shutdown -h now')
