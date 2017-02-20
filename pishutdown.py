7a49bc6b406b2b75870124f8de24227e92d69412
Andrewwango <andrew.wang27@gmail.com>
2017-02-20 19:24:50 +0000

    Update pishutdown.py

--- pishutdown.py
+++ pishutdown.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python2.7
 #a corresponding launcher .sh file is in /home/pi and launches this on boot.
-import os
+import os, time
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 
@@ -13,5 +13,7 @@ GPIO.wait_for_edge(SHUTBUTT, GPIO.FALLING)
 
 print "SHUTBUTT PRESSED, SHUTTING DOWN!!"
 GPIO.output(INDICLED, True)
+time.sleep(0.5)
 
+GPIO.cleanup()
 os.system('sudo shutdown -h now')
