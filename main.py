97ab9263f32ef0a579eb624b9631a7f60cbd63af
Andrewwango <andrew.wang27@gmail.com>
2017-02-21 20:40:04 +0000

    Update main.py

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ GRABBERPOWER   = -150
 OPENPOWER      = 70
 LIFTPOWER      = -160
 SLIDEUPPOWER   = -70
-BRINGDOWNPOWER = 120
+BRINGDOWNPOWER = 170
 BRINGDOWNBRAKEPOWER = -5
 
 ##SETUP## motors, sensors, GPIO Pins
@@ -275,7 +275,7 @@ def detectprocedure(alreadyturning):
 				else:
 					wheel1 = RWHEEL; wheel2 = LWHEEL
 				#use outside wheel to encode (although it doesn't matter)
-				movelimbENC(wheel1, -TURNPOWER, (int(tempElapsedTurningEnc/30)),wheel2, TURNPOWER)#fine tune this
+				movelimbENC(wheel1, -TURNPOWER, (int(tempElapsedTurningEnc/10)),wheel2, TURNPOWER)#fine tune this
 				movelimbLENG(wheel1, BRAKEPOWER, 0.1, wheel2, -BRAKEPOWER) #brake			
 				time.sleep(0.2)
 			
