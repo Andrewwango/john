4261dbde6e0e95b2e1da1cbf60d4b45f41dea9e1
Andrewwango <andrew.wang27@gmail.com>
2017-02-22 19:48:29 +0000

    Update pishutdown.py

--- pishutdown.py
+++ pishutdown.py
@@ -8,20 +8,32 @@ SHUTBUTT = 9
 BUZZOUT = 7
 GPIO.setup(SHUTBUTT, GPIO.IN , pull_up_down=GPIO.PUD_UP)
 
+#wait for button to be pressed
 GPIO.wait_for_edge(SHUTBUTT, GPIO.FALLING)
+
+#check if held or not
+time.sleep(2)
 
 #resetup in case it has been cleaned
 GPIO.cleanup()
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(BUZZOUT , GPIO.OUT)
+GPIO.setup(SHUTBUTT, GPIO.IN , pull_up_down=GPIO.PUD_UP)
 
-print "SHUTBUTT PRESSED, SHUTTING DOWN!!"
-for i in range(4):
+state = GPIO.input(SHUTBUTT)
+
+print "Shutbutt pressed!"
+for i in range(state+4):
 	GPIO.output(BUZZOUT, True)
 	time.sleep(0.1)
 	GPIO.output(BUZZOUT, False)
 	time.sleep(0.2)
 	
-
 GPIO.cleanup()
-os.system('sudo shutdown -h now')
+
+if state == 0: #button still held
+	print "BUTTON HELD: SHUTTING DOWN"
+	os.system("sudo shutdown -h now")
+elif state == 1: #button pressed and released
+	print "BUTTON PRESSED: REBOOTING"
+	os.system("sudo reboot")
