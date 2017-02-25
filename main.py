5cadb3820edfc1261578dbc007573235540daac2
Andrewwango <andrew.wang27@gmail.com>
2017-02-25 17:31:14 +0000

    Update main.py

--- main.py
+++ main.py
@@ -240,9 +240,12 @@ try:
 		if limb2 != None:
 			BrickPi.MotorSpeed[limb2] = 0
 
-	def turnwhilecondition(turnbackornot, trig, echo, op, val, power, wallprevention=False):
-		if turnbackornot == "turnback": b = 1
-		else: b = 0
+	def movewhilecondition(formofmovement, trig, echo, op, val, power, wallprevention=False):
+		wheel1power,wheel2power = -1,1 #for turning
+		if   formofmovement == "turnback":    b = 1
+		elif formofmovement == "notturnback": b = 0
+		elif formofmovement == "forwards": wheel1power,wheel2power = 1,1 #modify for driving
+		elif formofmovement == "backwards":wheel1power,wheel2power = -1,-1
 		#set wheel directions
 		if turnycount%2 == b: #direction is inputted by user
 			wheel1 = LWHEEL; wheel2 = RWHEEL
@@ -256,7 +259,7 @@ try:
 					break
 			
 			#turn until condition is met
-			BrickPi.MotorSpeed[wheel1] = -power; BrickPi.MotorSpeed[wheel2] = power
+			BrickPi.MotorSpeed[wheel1] = wheel1power*power; BrickPi.MotorSpeed[wheel2] = wheel2power*power
 			BrickPiUpdateValues()
 
 		movelimbLENG(wheel1, BRAKEPOWER, 0.1, wheel2, -BRAKEPOWER) #brake
@@ -287,12 +290,12 @@ try:
 
 				#DETECTION CORRECTION PROCEDURES
 				#if while turning, turn back a wee until it's in sight!
-				##(if this is not enough, turn back until it's in sigh then not, then turn back)
+				##(if this is not enough, turn back until it's in sight then not, then turn back)
 				if alreadyturning == True:
 					#turn back way it was turning, while it's not in sight (i.e. until it's in sight)
-					turnwhilecondition("turnback", USNEWTRIG, USNEWECHO, ">", USSTANDARD, SHIFTPOWER)
+					movewhilecondition("turnback", USNEWTRIG, USNEWECHO, ">", USSTANDARD, SHIFTPOWER)
 					#carry on turning a bit more until it's not in sight
-				#	turnwhilecondition("turnback", USNEWTRIG, USNEWECHO, "<", USSTANDARD)
+				#	movewhilecondition("turnback", USNEWTRIG, USNEWECHO, "<", USSTANDARD)
 					#shift back - zoning in
 				#	if turnycount%2 == 0: movelimbENC(LWHEEL, -TURNPOWER, 50, RWHEEL, TURNPOWER)
 				#	else:                 movelimbENC(RWHEEL, -TURNPOWER, 50, LWHEEL, TURNPOWER)				
@@ -302,31 +305,34 @@ try:
 				else:
 					#turn in a direction, until it's out of sight
 					print "Turning direction until no longer in sight"
-					turnwhilecondition("notturnback", USNEWTRIG, USNEWECHO, "<", USSTANDARD, SHIFTPOWER, wallprevention=True)
+					movewhilecondition("notturnback", USNEWTRIG, USNEWECHO, "<", USSTANDARD, SHIFTPOWER, wallprevention=True)
 					#turn back in the other direction until it's in sight again
 					print "Turning other direction until in sight again"
-					turnwhilecondition("turnback",    USNEWTRIG, USNEWECHO, ">", USSTANDARD, SHIFTPOWER)
+					movewhilecondition("turnback",    USNEWTRIG, USNEWECHO, ">", USSTANDARD, SHIFTPOWER)
 					#carry on, until it's out of sight
 					print "Turning other direction until no longer in sight"
-					turnwhilecondition("turnback",    USNEWTRIG, USNEWECHO, "<", USSTANDARD, SHIFTPOWER, wallprevention=True)
+					movewhilecondition("turnback",    USNEWTRIG, USNEWECHO, "<", USSTANDARD, SHIFTPOWER, wallprevention=True)
 					#shift a bit back; this centres John on the litter.
 					#print "shift"
 					#if turnycount%2 == 0: movelimbENC(LWHEEL, -TURNPOWER, SHIFTENC, RWHEEL, TURNPOWER)
 					#else:                 movelimbENC(RWHEEL, -TURNPOWER, SHIFTENC, LWHEEL, TURNPOWER)
 					#turn in original direction until it's in sight - now it's definitely in sight.
 					print "Turning in orginial direction to centre"
-					turnwhilecondition("notturnback", USNEWTRIG, USNEWECHO, ">", USSTANDARD, SHIFTPOWER)
+					movewhilecondition("notturnback", USNEWTRIG, USNEWECHO, ">", USSTANDARD, SHIFTPOWER)
 
 				#shooby closer/further if litter is not in optimum range to pick up
 				shoobied = 'no'
 				tempreading = takeusreading(USNEWTRIG, USNEWECHO)
+				startpos = takeencoderreading(LWHEEL)
 				if tempreading <= OPTLITTERRANGE[0]: #too close
 					print "too close, shoobying AWAY"
-					movelimbENC(LWHEEL, -WHEELPOWER, 160, RWHEEL, -WHEELPOWER)
+					movewhilecondition("backwards", USNEWTRIG, USNEWECHO, "<", OPTLITTERRANGE[0], WHEELPOWER)
+					shoobiedenc = abs(takeencoderreading(limb) - startpos) #so we know where we've gone
 					shoobied='away'
 				elif tempreading >= OPTLITTERRANGE[1]: #too far
 					print "too far, shoobying NEAR"
-					movelimbENC(LWHEEL, WHEELPOWER, 160, RWHEEL, WHEELPOWER)
+					movewhilecondition("forwards",  USNEWTRIG, USNEWECHO, ">", OPTLITTERRANGE[1], WHEELPOWER)
+					shoobiedenc = abs(takeencoderreading(limb) - startpos)
 					shoobied='near'
 
 				#PICK UP THE BLOODY LITTER
@@ -335,10 +341,10 @@ try:
 				#move back if shoobied before
 				if shoobied=='away':
 					print "shoobying NEAR back to original pos"
-					movelimbENC(LWHEEL, WHEELPOWER, 160, RWHEEL, WHEELPOWER)
+					movelimbENC(LWHEEL, WHEELPOWER, shoobiedenc, RWHEEL, WHEELPOWER)
 				elif shoobied=='near':
 					print "shoobying AWAY back to original pos"
-					movelimbENC(LWHEEL, -WHEELPOWER, 160, RWHEEL, -WHEELPOWER)
+					movelimbENC(LWHEEL, -WHEELPOWER, shoobiedenc, RWHEEL, -WHEELPOWER)
 
 				#turn back to original turnbear
 				adjust = targBear - takebearing()
@@ -367,7 +373,7 @@ try:
 				elif alreadyturning == True: #I am already turning so I want to get away from this goddamn wall
 					print "turning away from goddamn wall"
 					#turn until wall is no longer in sight (to get rid of stalling problem)(screw detection)
-					turnwhilecondition("notturnback", US2TRIG, US2ECHO, "<=", US2STANDARD, TURNPOWER)
+					movewhilecondition("notturnback", US2TRIG, US2ECHO, "<=", US2STANDARD, TURNPOWER)
 
 	def restartprogram(channel=0): #debounce interrupt, restart program
 		global debouncetimestamp
