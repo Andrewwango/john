bec34350df3449b94be239513453b91b2184c3f0
Andrewwango <andrew.wang27@gmail.com>
2017-03-10 18:33:31 +0000

    Update usnewtest2.py

--- testers/usnewtest2.py
+++ testers/usnewtest2.py
@@ -27,15 +27,15 @@ def takeusreading(trig, echo, repeats=3, disregardhigh=False): #take reading fro
 			while GPIO.input(echo) == 0:
 				start = time.time()
 				print "start"
-				if (start-stop) >= 0.006:
-					distance = DISTANCECUTOFF
-					break #bail out if waiting too long
+			#	if (start-stop) >= 0.006:
+			#		distance = DISTANCECUTOFF
+			#		break #bail out if waiting too long
 			while GPIO.input(echo) == 1:
 				stop  = time.time()
 				print "stop"
-				if (stop-start) >= 0.006:
-					distance = DISTANCECUTOFF
-					break #bail out if waiting too long
+			#	if (stop-start) >= 0.006:
+			#		distance = DISTANCECUTOFF
+			#		break #bail out if waiting too long
 			#breaks when cut off point is bypassed - this prevents stalling.
 			
 			duration = abs(stop - start)
